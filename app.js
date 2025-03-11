e167e21efb1efd21004729aeb72b08617204770d
Eliziane Passos <elizianepassos.oliveira@gmail.com>
2025-03-11 05:24:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let seuAmigoSecreto = sortearAmigo();
+
+function adicionarAmigo() {
+    let amigoIncluido = document.querySelector ('input').value;
+    
+    if (amigoIncluido == "") {
+        alert ('Por favor, inserir um nome');
+    } 
+    else {
+        amigos.push(amigoIncluido);
+        alert ("Amigo incluído")
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    amigoIncluido = document.querySelector('input');
+    amigoIncluido.value = '';
+}
+
+function sortearAmigo () {
+      return Math.random () * amigos.length;
+}
+
+console.log (seuAmigoSecreto);
+console.log (amigos);
